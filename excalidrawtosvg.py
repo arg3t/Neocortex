3dfa1faf9c3b38b8ce39b8a2746f58f2bd368008
Yigit Colakoglu <yigitcolakoglu@hotmail.com>
2021-11-16 19:39:26 +0000

    Updates for excalidraw

---
+++ excalidrawtosvg.py
@@ -0,0 +1,59 @@
+#!/bin/python
+#
+# excalidrawtosvg.py
+# Neocortex
+#
+# Created by Yigit Colakoglu on 11/16/21.
+# Copyright 2021. Yigit Colakoglu. All rights reserved.
+#
+
+import os
+import re
+
+def findandreplace(directory, query, result, extension):
+    reg = re.compile(query)
+    for n in os.listdir(directory):
+        d = os.path.join(directory, n)
+        if os.path.isdir(d):
+            findandreplace(d, query, result, extension)
+            continue
+
+        if not n.endswith("."+extension):
+            continue
+
+        print(d)
+        with open(d,"r") as f:
+            data = f.read()
+
+        data = reg.sub(result, data)
+
+        with open(d,"w") as f:
+            f.write(data)
+
+def main():
+    excalidir = "notes/Excalidraw/"
+    for n in os.listdir(excalidir):
+        with open(os.path.join(excalidir, n), "r") as f:
+            lines = f.readlines()
+
+        insvg = False
+        svg = ""
+        for i in lines:
+            if insvg:
+                svg += i.strip()
+            if i == '```html\n':
+                insvg = True
+            elif i == '```\n':
+                insvg = False
+
+        svgname = ".".join(n.split(".")[0:-1]) + ".svg"
+
+        with open(os.path.join(excalidir, svgname), "w") as f:
+            f.write(svg)
+
+    findandreplace("notes", r"!\[\[(Excalidraw\/Drawing.*).md\]\]", r"![Excalidraw Drawing](/\1.svg)", "md")
+
+
+if __name__ == "__main__":
+    main()
+
